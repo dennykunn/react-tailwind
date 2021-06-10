ece7691b8f3c7e1c63cfb6e796e4e94e82b5ca0f
dennykunn <60263420+dennykunn@users.noreply.github.com>
2021-06-11 02:57:02 +0700

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,8 +5,8 @@ import ImageTwo from '../images/egg-2.jpg'
 const Content = () => {
     return (
         <>
-            <div className='menu-card'>
-                <img src={ImageOne} alt='' id='#product-one' className='h-full rounded mb-20 shadow'/>
+            <div className='menu-card' id='#product-one'>
+                <img src={ImageOne} alt='' className='h-full rounded mb-20 shadow'/>
                 <div className='center-content'>
                     <h2 className='text-2xl mb-2'>Egg Muffin</h2>
                     <p className='mb-2'>Crispy, delicious, and nutricious</p>
